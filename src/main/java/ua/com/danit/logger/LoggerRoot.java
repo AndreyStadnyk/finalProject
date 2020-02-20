ff2d66260e6d7618184ac5ea0080b387bf4e4312
ShevchukMarina <shev.marinet@gmail.com>
2020-02-21 01:30:44 +0200

    Added logger to console

---
+++ src/main/java/ua/com/danit/logger/LoggerRoot.java
@@ -0,0 +1,12 @@
+package ua.com.danit.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerRoot {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerRoot.class);
+
+    public void printAction(String action){
+        logger.info("Logger action: " + action);
+    }
+}
