57d3db6db01c3e97445337b00521925d8d7a2fe4
vetalkonev <vetalkonev13@gmail.com>
2020-02-24 13:33:08 +0200

    Sign UP

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import React, {useEffect, useState} from 'react'
 import './App.css'
-import axios from 'axios'
+import SignIn from './components/SignIn'
+// import axios from 'axios'
 
 function App () {
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    axios('/api/v1/users/current')
-      .then(response => {
-        setUser(response.data)
-      })
-  }, [])
-
-  if (!user) {
-    return 'Loading...'
-  }
+  // const [user, setUser] = useState(null)
+  //
+  // useEffect(() => {
+  //   axios('/api/v1/users/current')
+  //     .then(response => {
+  //       setUser(response.data)
+  //     })
+  // }, [])
+  //
+  // if (!user) {
+  //   return 'Loading...'
+  // }
 
   return (
-    <div className='App'>
-      <h2>Hello, {user.firstName} {user.lastName}</h2>
-    </div>
+    <SignIn/>
+
   )
 }
 
