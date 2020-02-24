c8e1f9de2ad1fe5a89fed29c6229b922a9fc01a9
vetalkonev <vetalkonev13@gmail.com>
2020-02-24 16:23:20 +0200

    react router dom
    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import SignIn from './components/SignIn'
+import SignIn from './components/sign-in'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // import axios from 'axios'
 
 function App () {
@@ -18,7 +19,13 @@ function App () {
   // }
 
   return (
-    <SignIn/>
+
+    <Switch>
+      <Route exact path='/sign-in' component={SignIn}/>
+      {/*
+      <Route exact path='/profile/:id' component={MyAccountPage} />
+      */}
+    </Switch>
 
   )
 }
