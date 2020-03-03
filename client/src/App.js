07c4a66e8d9d687c75b5b0fb2c463f7a7b456a84
vetalkonev <40244582+vetalkonev@users.noreply.github.com>
2020-03-03 19:23:57 +0200

    Vk profile page (#23)
    * profile page
    
    * Update profile.js
    
    * profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
-import React, {useEffect, useState} from 'react'
+// import React, { useEffect, useState } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
-import {Route, Switch} from "react-router-dom";
-import Tape from "./components/Tape/Tape";
-import SignIn from "./components/sign-in";
+import Axios from 'axios'
+import Tape from './components/Tape/Tape'
+import SignIn from './components/SignIn/sign-in'
+import Profile from './components/Profile/profile'
 
-export default function App() {
-    const [user, setUser] = useState(null);
+export default function App () {
+  const [user, setUser] = useState(null)
 
-    useEffect(() => {
-        axios('/api/v1/users/current')
-            .then(response => {
-                setUser(response.data)
-            })
-    }, []);
+  useEffect(() => {
+    Axios('/api/v1/users/current')
+      .then(response => {
+        setUser(response.data)
+      })
+  }, [])
 
-    if (!user) {
-        return (
-    <div>
+  if (!user) {
+    return (
+      <div>
         <Switch>
-            <Route exact path='/sign-in' component={SignIn}/>
-
-            <Route exact path='/tape' component={Tape}/>
-
-
+          <Route exact path='/sign-in' component={SignIn}/>
+          <Route exact path='/profile' component={Profile}/>
+          <Route exact path='/tape' component={Tape}/>
+          <Route exact path='/*' component={SignIn}/>
         </Switch>
-    </div>
+      </div>
 
-        )
-    }
+    )
+  }
 }
-
