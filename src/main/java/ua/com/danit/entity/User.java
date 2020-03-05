78ead770a1e54182e38cca43d90836bfc5c69081
ADmin <interra.com.ua@gmail.com>
2020-03-05 17:00:11 +0200

    style of js files fixed

--- src/main/java/ua/com/danit/entity/User.java
+++ src/main/java/ua/com/danit/entity/User.java
@@ -2,7 +2,16 @@ package ua.com.danit.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.CascadeType;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
