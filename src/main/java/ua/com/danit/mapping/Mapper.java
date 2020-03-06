46841e37948fe95102f6b50d054756fb6a0e0452
alex5272372 <alex5272372@gmail.com>
2020-03-06 17:57:20 +0200

    Checkstyle fix

--- src/main/java/ua/com/danit/mapping/Mapper.java
+++ src/main/java/ua/com/danit/mapping/Mapper.java
@@ -6,5 +6,6 @@ import ua.com.danit.entity.AbstractEntity;
 // https://github.com/promoscow/modelmapper-demo
 public interface Mapper<E extends AbstractEntity, D extends AbstractDto> {
   E toEntity(D dto);
+
   D toDto(E entity);
 }
