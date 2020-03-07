195d70908422a4b6ade15df47a2fbee0661adb46
Andrey Stadnyk <44724978+AndreyStadnyk@users.noreply.github.com>
2020-03-07 23:11:58 +0200

    added new api: return current user, update current user (#28)

--- src/main/java/ua/com/danit/controller/UserController.java
+++ src/main/java/ua/com/danit/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ua.com.danit.dto.request.UserRequest;
@@ -27,8 +28,19 @@ public class UserController {
     return ResponseEntity.ok(userMapper.create(userRequest));
   }
 
+  @PutMapping
+  public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest) {
+    return ResponseEntity.ok(userMapper.updateUser(userRequest));
+  }
+
   @GetMapping("/{username}")
   public ResponseEntity<UserResponse> getUserByUsername(@PathVariable String username) {
     return ResponseEntity.ok(userMapper.findById(username));
   }
+
+  @GetMapping("/current")
+  public ResponseEntity<UserResponse> getCurrentUser() {
+    return ResponseEntity.ok(userMapper.getCurrentUser());
+  }
+
 }
