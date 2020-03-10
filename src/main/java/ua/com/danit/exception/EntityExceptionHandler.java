9e363102b847ead4f3fa26b3bd9d725ad5163e3c
vetalkonev <vetalkonev13@gmail.com>
2020-03-10 13:48:59 +0200

    Update EntityExceptionHandler.java

--- src/main/java/ua/com/danit/exception/EntityExceptionHandler.java
+++ src/main/java/ua/com/danit/exception/EntityExceptionHandler.java
@@ -17,7 +17,7 @@ public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(Exception.class)
   public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
     List<String> details = new ArrayList<>();
-    details.add(ex.getLocalizedMessage());
+    details.add(ex.getMessage());
     ErrorResponse error = new ErrorResponse("Server Error", details);
     return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
   }
