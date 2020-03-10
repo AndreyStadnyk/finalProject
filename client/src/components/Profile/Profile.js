392e2116c56303c5b5d7541b8f329c6eb61b7814
alex5272372 <alex5272372@gmail.com>
2020-03-10 19:18:54 +0200

    Delete import

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
-import { connect, useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Post from '../Post/Post'
 import { updateUser } from '../../actions/profileActions'
 
