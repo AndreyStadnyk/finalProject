60082db0dcf01bb8bf06b0a15d9ce3478eb4648e
alex5272372 <alex5272372@gmail.com>
2020-03-11 15:50:06 +0200

    Imports and eslint

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
-import Profile2 from '../Profile/Profile'
+import Profile2 from '../Profile/Profile2'
 import Tape from '../Tape/Tape'
 import NotFound from '../NotFound/NotFound'
 import { useDispatch, useSelector } from 'react-redux'
