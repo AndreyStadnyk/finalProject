16ab0b285e946b5a270d533eb6cd310ef5624fcd
vetalkonev <vetalkonev13@gmail.com>
2020-03-11 10:25:34 +0200

    commit

--- client/src/components/Profile/ProfileTape.js
+++ client/src/components/Profile/ProfileTape.js
@@ -15,7 +15,7 @@ const overlay = {
   right: 10
 }
 const tape = {
-  maxWidth: 800,
+
   minHeight: 450
 }
 
@@ -50,7 +50,7 @@ class ProfileTape extends Component {
           title: 'This is the 4th Title',
           content:
 
-            'This is the 4th content of the application, this is a long content that needs to be show partially',
+          'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
           likes: 5,
           comments: 10,
           views: 14
