71b277bee41f0fa3e1eea594cfffc37d5291b5a7
vetalkonev <vetalkonev13@gmail.com>
2020-03-13 14:59:42 +0200

    profile

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
-import Profile2 from '../Profile/Profile2'
+import Profile from '../Profile/Profile'
 import Tape from '../Tape/Tape'
 import NotFound from '../NotFound/NotFound'
 import { useDispatch, useSelector } from 'react-redux'
 import api from '../../helpers/FetchData'
 import { profileTypes } from '../../actions/profileActions'
 
-export default function ProtectedRouter (props) {
+export default function ProtectedRouter () {
   const dispatch = useDispatch()
   const { currentUser } = useSelector(state => ({ currentUser: state.users.currentUser }))
 
@@ -25,7 +25,7 @@ export default function ProtectedRouter (props) {
   if (currentUser) {
     return (
       <Switch>
-        <Route exact path='/profile' component={Profile2}/>
+        <Route exact path='/profile' component={Profile}/>
         <Route exact path='/tape' component={Tape}/>
         <Route exact path='/*' component={NotFound}/>
       </Switch>
