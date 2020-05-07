03e1ee4b72639cd19c65b5cb90c9b837e9216afb
vetalkonev <vetalkonev13@gmail.com>
2020-05-07 21:27:06 +0300

    Revert "Revert "Update Post.js""
    This reverts commit f191f4508bc124d4ff267e87064ae7d041dbbc6a.

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Avatar from 'material-ui/Avatar'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 import EditIcon from '@material-ui/icons/Edit'
 import './Post.css'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
-import {pink, lightBlue} from '@material-ui/core/colors'
-import {deletePost} from '../../actions/postActions'
-import {useDispatch} from 'react-redux'
+import { pink, lightBlue } from '@material-ui/core/colors'
+import { deletePost } from '../../actions/postActions'
+import { useDispatch } from 'react-redux'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import CardContent from '@material-ui/core/CardContent'
 import Card from '@material-ui/core/Card'
@@ -15,6 +15,8 @@ import ModalWindow from '../ModalPost/ModalPost'
 import Comment from '../Comment/Comment'
 import Button from '@material-ui/core/Button'
 import ModalComment from '../ModalComment/ModalComment'
+import Tooltip from '@material-ui/core/Tooltip'
+import LikeIcon from '@material-ui/icons/Favorite'
 
 export default function Post (props) {
   const dispatch = useDispatch()
@@ -36,7 +38,8 @@ export default function Post (props) {
   const modal = modalActive
     ? <ModalWindow modalActive={modalActive} post={props.post} setActive={setActive}/> : null
   const commentModal = commentModalActive ? <ModalComment commentModalActive={commentModalActive}
-    postId={props.post.id} setCommentActive={setCommentActive}/> : null
+                                                          postId={props.post.id}
+                                                          setCommentActive={setCommentActive}/> : null
 
   const useStyles = makeStyles(theme => ({
     root: {
@@ -88,10 +91,10 @@ export default function Post (props) {
           <CardContent className={classes.content}>
             <div className={classes.text}>
               <Typography component="p" variant="subtitle2">
-                  Owner: {props.post.ownerUsername}
+                Owner: {props.post.ownerUsername}
               </Typography>
               <Typography component="p" variant="subtitle2">
-                  Author: {props.post.authorUsername}
+                Author: {props.post.authorUsername}
               </Typography>
               <Typography component="p" variant="subtitle2">
                 {props.post.date.toString()}
@@ -100,7 +103,7 @@ export default function Post (props) {
                 {props.post.text}
               </Typography>
               <Typography component="p" variant="h5">
-                  Comments:
+                Comments:
               </Typography>
               <Button
                 variant="contained"
@@ -108,25 +111,38 @@ export default function Post (props) {
                 className={classes.button}
                 onClick={toggleCommentModal}
               >
-                  Add
+                Add
               </Button>
               {props.post.comments.map(comment => (
                 <Comment comment={comment} postId={props.post.id}>
                 </Comment>
               ))}
-              <Typography component="p" variant="h5">
-                  Likes ({props.post.likes.length}):
-              </Typography>
-              {props.post.likes.map(like => (
+              <Tooltip title={props.post.likes.map(like => (
                 <Typography component="p" variant="body2">
                   {like.userUsername}
                 </Typography>
-              ))}
+              ))} arrow>
+                <Typography component="p" variant="h5">
+                  Likes ({props.post.likes.length}):
+                </Typography>
+              </Tooltip>
             </div>
           </CardContent>
+
           <IconButton
             className={classes.button}
-            style={{color: lightBlue.A700}}
+            style={{ color: lightBlue.A700 }}
+            onClick={e => {
+              e.stopPropagation()
+              toggleModal()
+            }}
+          >
+            <LikeIcon/>
+          </IconButton>
+
+          <IconButton
+            className={classes.button}
+            style={{ color: lightBlue.A700 }}
             onClick={e => {
               e.stopPropagation()
               toggleModal()
@@ -137,7 +153,7 @@ export default function Post (props) {
 
           <IconButton
             className={classes.button}
-            style={{color: pink[500]}}
+            style={{ color: pink[500] }}
             onClick={e => {
               e.stopPropagation()
               handleClickDelete()
