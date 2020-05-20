d3deae4b925eceee548a657eccc4039b50ea1dbc
emadhamad171 <dusikeh@gmail.com>
2020-05-20 18:23:43 +0300

    user log out fixed 100% working

--- src/main/java/ua/com/danit/security/WebSecurityConfig.java
+++ src/main/java/ua/com/danit/security/WebSecurityConfig.java
@@ -17,47 +17,47 @@ import ua.com.danit.service.UserService;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserService userService;
-    private SecurityFailureHandler failureHandler;
-    private SecuritySuccessHandler successHandler;
-    private AuthenticationEntryPoint authenticationEntryPoint;
+  private UserService userService;
+  private SecurityFailureHandler failureHandler;
+  private SecuritySuccessHandler successHandler;
+  private AuthenticationEntryPoint authenticationEntryPoint;
 
-    @Autowired
-    WebSecurityConfig(UserService userService,
-                      SecuritySuccessHandler successHandler,
-                      SecurityFailureHandler failureHandler,
-                      AuthenticationEntryPoint authenticationEntryPoint) {
-        this.userService = userService;
-        this.failureHandler = failureHandler;
-        this.successHandler = successHandler;
-        this.authenticationEntryPoint = authenticationEntryPoint;
-    }
+  @Autowired
+  WebSecurityConfig(UserService userService,
+                    SecuritySuccessHandler successHandler,
+                    SecurityFailureHandler failureHandler,
+                    AuthenticationEntryPoint authenticationEntryPoint) {
+    this.userService = userService;
+    this.failureHandler = failureHandler;
+    this.successHandler = successHandler;
+    this.authenticationEntryPoint = authenticationEntryPoint;
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/h2-console/**", "/swagger-ui.html", "/api/users",
-                        "/api/users/current", "/api/users/resetPassword", "/api/users/changePassword")
-                .permitAll()
-                .antMatchers("/api/**")
-                .authenticated()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint)
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/auth")
-                .successHandler(successHandler)
-                .failureHandler(failureHandler)
-                .permitAll()
-                .and()
-                .logout() .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
-                        http.headers().frameOptions().disable();
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests()
+      .antMatchers("/h2-console/**", "/swagger-ui.html", "/api/users",
+        "/api/users/current", "/api/users/resetPassword", "/api/users/changePassword")
+      .permitAll()
+      .antMatchers("/api/**")
+      .authenticated()
+      .and()
+      .exceptionHandling()
+      .authenticationEntryPoint(authenticationEntryPoint)
+      .and()
+      .formLogin()
+      .loginProcessingUrl("/auth")
+      .successHandler(successHandler)
+      .failureHandler(failureHandler)
+      .permitAll()
+      .and()
+      .logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
+    http.headers().frameOptions().disable();
+  }
 
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return userService;
-    }
+  @Override
+  protected UserDetailsService userDetailsService() {
+    return userService;
+  }
 }
 
